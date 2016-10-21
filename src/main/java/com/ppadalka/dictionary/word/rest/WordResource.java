4bf94b03463fbc8b5d0c60089c861fe415e19bb6
ppadalka <ppadalka@inbox.ru>
2016-10-22 01:11:39 +0300

    #16 - made rest api more clear and strong

--- src/main/java/com/ppadalka/dictionary/word/rest/WordResource.java
+++ src/main/java/com/ppadalka/dictionary/word/rest/WordResource.java
@@ -5,6 +5,7 @@ import com.ppadalka.dictionary.word.view.WordView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/api/word")
+@RequestMapping(path = "/words")
 public class WordResource {
     private final WordService wordService;
 
@@ -24,23 +25,23 @@ public class WordResource {
         this.wordService = wordService;
     }
 
-    @GetMapping(path = "/{wordId}")
+    @GetMapping(path = "/{wordId}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public WordView get(@PathVariable("wordId") Long id) {
         return wordService.findOne(id);
     }
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Page<WordView> getPage(@RequestParam(value = "lang", required = false, defaultValue = "ENG") String language,
                                       Pageable pageable) {
         return wordService.findAll(pageable);
     }
 
-    @PostMapping
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public WordView save(@RequestBody WordView wordView) {
         return wordService.create(wordView);
     }
 
-    @PutMapping
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public WordView update(@RequestBody WordView wordView) {
         return wordService.update(wordView);
     }

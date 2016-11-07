41d66adb443318834c0aa728a4e06510048803fc
ppadalka <ppadalka@inbox.ru>
2016-11-07 15:24:14 +0200

    #33 - added word view

--- src/main/resources/static/src/scripts/word/view/word.view.js
+++ src/main/resources/static/src/scripts/word/view/word.view.js
@@ -1,10 +1,14 @@
 'use strict';
 
 define(function (require) {
+
+    require('word.template');
+
     var BaseView = require('base.view');
 
     var WordView = BaseView.extend({
 
+        template: 'word.template'
     });
 
     return WordView;

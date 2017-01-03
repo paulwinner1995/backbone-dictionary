36ab58dc8954b05865ad50281e46a4a64c8c48dc
ppadalka <ppadalka@inbox.ru>
2017-01-03 16:35:46 +0200

    #77 - fixed pagination data displaying

--- src/main/resources/static/src/scripts/common/collection/base.pageable.collection.js
+++ src/main/resources/static/src/scripts/common/collection/base.pageable.collection.js
@@ -13,17 +13,37 @@ var BasePageableCollection = BackbonePageableCollection.extend({
         order: -1,
 
         // extra param
-        isFirst: true,
-        isLast: true,
+        from: 0,
+        to: 0
     },
 
     queryParams: {
-        currentPage: 'page',
         pageSize: 'size',
+    },
+
+    parseState: function (response) {
+        var from = this._startFrom(response.totalElements, response.size, response.number);
+        var to = this._endWith(response.totalElements, from, response.numberOfElements);
+
+        return {
+            totalPages: response.totalPages,
+            totalRecords: response.totalElements,
+            // set extra param
+            from: from,
+            to: to
+        };
     },
 
     parseRecords: function (response) {
         return response.content;
+    },
+
+    _startFrom: function (totalRecords, pageSize, currentPage) {
+        return totalRecords ? pageSize * currentPage + 1 : 0;
+    },
+    
+    _endWith: function (totalRecords, from, numberOfElements) {
+        return totalRecords ? from + numberOfElements - 1 : 0;
     }
 });
 
